66be1f6ca3a2f16e530279e1c8e0a5ec6ea4d7d7
Nicolas Patry <patry.nicolas@protonmail.com>
2022-09-29 15:58:07 +0200

    Bump version number for next release.

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1.dev0"
 
 # Re-export this
 from .safetensors_rust import safe_open
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -10,7 +10,7 @@ extras["docs"] = ["sphinx", "sphinx_rtd_theme", "setuptools_rust"]
 
 setup(
     name="safetensors",
-    version="0.2.0",
+    version="0.2.1.dev0",
     description="Fast and Safe Tensor serialization",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
