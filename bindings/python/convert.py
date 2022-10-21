945abd305b1fdc8ff270f128a1027f8251ebce70
Nicolas Patry <patry.nicolas@protonmail.com>
2022-10-21 09:09:55 +0200

    Updating convert script + fixing index. (#29)
    * Updating convert script + fixing index.
    
    * Cleaning even when exceptions occur.

--- bindings/python/convert.py
+++ bindings/python/convert.py
@@ -1,53 +1,72 @@
 import argparse
 import json
+import os
 
 import torch
 
 from huggingface_hub import CommitOperationAdd, HfApi, hf_hub_download
-from huggingface_hub.utils import EntryNotFoundError
 from safetensors.torch import save_file
 
 
+def rename(pt_filename) -> str:
+    local = pt_filename.replace(".bin", ".safetensors")
+    local = local.replace("pytorch_model", "model")
+    return local
+
+
 def convert_multi(model_id):
-    filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin.index.json")
-    with open(filename, "r") as f:
-        data = json.load(f)
-
-    filenames = set(data["weight_map"].values())
     local_filenames = []
-    for filename in filenames:
-        cached_filename = hf_hub_download(repo_id=model_id, filename=filename)
-        loaded = torch.load(cached_filename)
-        local = filename.replace(".bin", ".safetensors")
-        local = local.replace("pytorch_model", "model")
-        save_file(loaded, local, metadata={"format": "pt"})
-        local_filenames.append(local)
+    try:
+        filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin.index.json")
+        with open(filename, "r") as f:
+            data = json.load(f)
 
-    api = HfApi()
-    operations = [CommitOperationAdd(path_in_repo=local, path_or_fileobj=local) for local in local_filenames]
-    api.create_commit(
-        repo_id=model_id,
-        operations=operations,
-        commit_message="Adding `safetensors` variant of this model",
-        create_pr=True,
-    )
+        filenames = set(data["weight_map"].values())
+        for filename in filenames:
+            cached_filename = hf_hub_download(repo_id=model_id, filename=filename)
+            loaded = torch.load(cached_filename)
+            local = rename(filename)
+            save_file(loaded, local, metadata={"format": "pt"})
+            local_filenames.append(local)
+
+        index = "model.safetensors.index.json"
+        with open(index, "w") as f:
+            newdata = {k: v for k, v in data.items()}
+            newmap = {k: rename(v) for k, v in data["weight_map"].items()}
+            newdata["weight_map"] = newmap
+            json.dump(newdata, f)
+        local_filenames.append(index)
+
+        api = HfApi()
+        operations = [CommitOperationAdd(path_in_repo=local, path_or_fileobj=local) for local in local_filenames]
+        api.create_commit(
+            repo_id=model_id,
+            operations=operations,
+            commit_message="Adding `safetensors` variant of this model",
+            create_pr=True,
+        )
+    finally:
+        for local in local_filenames:
+            os.remove(local)
 
 
 def convert_single(model_id):
-    filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin")
-
-    loaded = torch.load(filename)
     local = "model.safetensors"
-    save_file(loaded, local, metadata={"format": "pt"})
+    try:
+        filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin")
+        loaded = torch.load(filename)
+        save_file(loaded, local, metadata={"format": "pt"})
 
-    api = HfApi()
+        api = HfApi()
 
-    api.upload_file(
-        path_or_fileobj=local,
-        create_pr=True,
-        path_in_repo=local,
-        repo_id=model_id,
-    )
+        api.upload_file(
+            path_or_fileobj=local,
+            create_pr=True,
+            path_in_repo=local,
+            repo_id=model_id,
+        )
+    finally:
+        os.remove(local)
 
 
 if __name__ == "__main__":
@@ -65,7 +84,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     model_id = args.model_id
-    try:
-        convert_multi(model_id)
-    except EntryNotFoundError:
+    api = HfApi()
+    info = api.model_info(model_id)
+    filenames = set(s.rfilename for s in info.siblings)
+    if "pytorch_model.bin" in filenames:
         convert_single(model_id)
+    else:
+        convert_multi(model_id)
