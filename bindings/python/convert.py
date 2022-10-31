2abf8f69a671e6bde79268309ba27d60ff1eebda
Nicolas Patry <patry.nicolas@protonmail.com>
2022-10-31 10:30:49 +0000

    Hardened convert script.

--- bindings/python/convert.py
+++ bindings/python/convert.py
@@ -1,10 +1,14 @@
 import argparse
 import json
 import os
+import shutil
 
 import torch
 
 from huggingface_hub import CommitOperationAdd, HfApi, hf_hub_download
+from huggingface_hub.file_download import repo_folder_name
+from transformers import AutoConfig
+from transformers.pipelines.base import infer_framework_load_model
 from safetensors.torch import save_file
 
 
@@ -13,7 +17,7 @@ def check_file_size(sf_filename, pt_filename):
     pt_size = os.stat(pt_filename).st_size
 
     if (sf_size - pt_size) / pt_size > 0.01:
-        raise ValueError(f"""The file size different is more than 1%:
+        raise RuntimeError(f"""The file size different is more than 1%:
          - {sf_filename}: {sf_size}
          - {pt_filename}: {pt_size}
          """)
@@ -26,88 +30,89 @@ def rename(pt_filename) -> str:
 
 
 def convert_multi(model_id):
-    local_filenames = []
-    try:
-        filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin.index.json")
-        with open(filename, "r") as f:
-            data = json.load(f)
+    filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin.index.json")
+    with open(filename, "r") as f:
+        data = json.load(f)
 
-        filenames = set(data["weight_map"].values())
-        for filename in filenames:
-            cached_filename = hf_hub_download(repo_id=model_id, filename=filename)
-            loaded = torch.load(cached_filename)
-            local = rename(filename)
-            save_file(loaded, local, metadata={"format": "pt"})
-            check_file_size(local, cached_filename)
-            local_filenames.append(local)
+    filenames = set(data["weight_map"].values())
+    for filename in filenames:
+        cached_filename = hf_hub_download(repo_id=model_id, filename=filename)
+        loaded = torch.load(cached_filename)
+        sf_filename = rename(filename)
 
-        index = "model.safetensors.index.json"
-        with open(index, "w") as f:
-            newdata = {k: v for k, v in data.items()}
-            newmap = {k: rename(v) for k, v in data["weight_map"].items()}
-            newdata["weight_map"] = newmap
-            json.dump(newdata, f)
-        local_filenames.append(index)
-
-        api = HfApi()
-        operations = [CommitOperationAdd(path_in_repo=local, path_or_fileobj=local) for local in local_filenames]
-
-        return True
-        api.create_commit(
-            repo_id=model_id,
-            operations=operations,
-            commit_message="Adding `safetensors` variant of this model",
-            create_pr=True,
-        )
-    except ValueError as e:
-        print(f"Error {model_id}: {e}")
-    finally:
-        for local in local_filenames:
-            try:
-                os.remove(local)
-            except FileNotFoundError:
-                pass
-
-
-def convert_single(model_id):
-    local = "model.safetensors"
-    try:
-        filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin")
-        loaded = torch.load(filename)
+        local = os.path.join(folder, sf_filename)
         save_file(loaded, local, metadata={"format": "pt"})
-        check_file_size(local, filename)
+        check_file_size(local, cached_filename)
+        local_filenames.append(local)
 
-        api = HfApi()
+    index = os.path.join(folder, "model.safetensors.index.json")
+    with open(index, "w") as f:
+        newdata = {k: v for k, v in data.items()}
+        newmap = {k: rename(v) for k, v in data["weight_map"].items()}
+        newdata["weight_map"] = newmap
+        json.dump(newdata, f)
+    local_filenames.append(index)
+
+    operations = [CommitOperationAdd(path_in_repo=local.split("/")[-1], path_or_fileobj=local) for local in local_filenames]
+
+    return operations
 
 
-        return True
+def convert_single(model_id, folder):
+    sf_filename = "model.safetensors"
+    filename = hf_hub_download(repo_id=model_id, filename="pytorch_model.bin")
+    loaded = torch.load(filename)
 
-        api.upload_file(
-            path_or_fileobj=local,
-            create_pr=True,
-            path_in_repo=local,
-            repo_id=model_id,
-        )
-    except ValueError as e:
-        print(f"Error {model_id}: {e}")
-    finally:
-        try:
-            os.remove(local)
-        except FileNotFoundError:
-            pass
+    local = os.path.join(folder, sf_filename)
+    save_file(loaded, local, metadata={"format": "pt"})
+
+    check_file_size(local, filename)
+
+    operations = [CommitOperationAdd(path_in_repo=sf_filename, path_or_fileobj=local)]
+    return operations
+
+def check_final_model(model_id, folder):
+    config = hf_hub_download(repo_id=model_id, filename="config.json")
+    shutil.copy(config, os.path.join(folder, "config.json"))
+    config = AutoConfig.from_pretrained(folder)
+    _, sf_model = infer_framework_load_model(folder, config)
+    _, pt_model = infer_framework_load_model(model_id, config)
+
+    input_ids = torch.arange(10).long().unsqueeze(0)
+    sf_logits = sf_model(input_ids)
+    pt_logits = pt_model(input_ids)
+    torch.testing.assert_close(sf_logits, pt_logits)
+    print(f"Model {model_id} is ok !")
 
 
 def convert(api, model_id):
     info = api.model_info(model_id)
     filenames = set(s.rfilename for s in info.siblings)
 
-    if "model.safetensors" in filenames or "model_index.safetensors.index.json" in filenames:
-        print(f"Model {model_id} is already converted, skipping..")
-        return 1
-    if "pytorch_model.bin" in filenames:
-        convert_single(model_id)
-    else:
-        convert_multi(model_id)
+    folder = repo_folder_name(repo_id=model_id, repo_type="models")
+    os.makedirs(folder)
+    try:
+        operations = None
+        if "model.safetensors" in filenames or "model_index.safetensors.index.json" in filenames:
+            print(f"Model {model_id} is already converted, skipping..")
+        elif "pytorch_model.bin" in filenames:
+            operations = convert_single(model_id, folder)
+        elif "pytorch_model.bin.index.json" in filenames:
+            operations = convert_multi(model_id, folder)
+        else:
+            raise RuntimeError(f"Model {model_id} doesn't seem to be a valid pytorch model. Cannot convert")
+
+        if operations:
+            check_final_model(model_id, folder)
+            api.create_commit(
+                repo_id=model_id,
+                operations=operations,
+                commit_message="Adding `safetensors` variant of this model",
+                create_pr=True,
+            )
+    finally:
+        shutil.rmtree(folder)
+    return 1
 
 
 if __name__ == "__main__":
--- bindings/python/convert_all.py
+++ bindings/python/convert_all.py
@@ -11,13 +11,25 @@ if __name__ == "__main__":
     models = list(api.list_models(filter=ModelFilter(library=args.library.Transformers), sort="downloads", direction=-1))[:total]
 
     correct = 0
+    file_diff = set()
+    not_contiguous = set()
     for model in models:
+        model_id = model.modelId
         print(f"[{model.downloads}] {model.modelId}")
         try:
-            result = convert(api, model.modelId)
+            result = convert(api, model_id)
+            print(result)
             if result is not None:
                 correct += 1
+        except RuntimeError as e:
+            file_diff.add( model_id)
+            print(e)
         except ValueError as e:
+            not_contiguous.add(model_id)
             print(e)
 
+    print(f"Not contiguous: {not_contiguous}")
+    print(f"File size is difference {file_diff}")
+    print(f"Not contiguous: {len(not_contiguous)}")
+    print(f"File size is difference {len(file_diff)}")
     print(f"Correct rate {correct}/{total} ({correct/total * 100:.2f}%)")
