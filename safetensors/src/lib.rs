fda385b693ba5710a25b715316a8c3780e4e81b9
Thomas Wang <24695242+thomasw21@users.noreply.github.com>
2022-12-14 11:23:50 +0100

    Update README.md (#131)
    * Update README.md
    
    * Update other files.
    
    Co-authored-by: Nicolas Patry <patry.nicolas@protonmail.com>

--- safetensors/src/lib.rs
+++ safetensors/src/lib.rs
@@ -53,8 +53,8 @@
 //!
 //! - 8 bytes: `N`, a u64 int, containing the size of the header
 //! - N bytes: a JSON utf-8 string representing the header.
-//!         The header is a dict like {"TENSOR_NAME": {"dtype": "float16", "shape": [1, 16, 256], "offsets": (X, Y)}}, where X and Y are the offsets in the byte buffer of the tensor data
-//!         A special key `__metadata__` is allowed to contain free form text map.
+//!   - The header is a dict like {"TENSOR_NAME": {"dtype": "float16", "shape": [1, 16, 256], "offsets": (X, Y)}}, where X and Y are the offsets in the byte buffer of the tensor data
+//!   - A special key `__metadata__` is allowed to contain free form text map.
 //! - Rest of the file: byte-buffer.
 //!
 //!
