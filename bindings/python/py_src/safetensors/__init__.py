6c9e191bb67f386f8fc6d229fab7a60dbaa690af
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-19 11:55:04 +0100

    Fixing Windows issue by actually dropping the rust resources.

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -1,4 +1,20 @@
 __version__ = "0.2.9"
 
 # Re-export this
-from .safetensors_rust import safe_open  # noqa: F401
+from .safetensors_rust import safe_open as rust_open  # noqa: F401
+
+
+class safe_open:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __getattr__(self, __name: str):
+        return getattr(self.f, __name)
+
+    def __enter__(self):
+        self.f = rust_open(*self.args, **self.kwargs)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        del self.f
