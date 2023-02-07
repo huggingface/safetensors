cb779f001e163b8856b5c5251bcca13d51bc12d5
Nicolas Patry <patry.nicolas@protonmail.com>
2023-02-07 15:32:54 +0100

    Style.

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.2.9"
 
 # Re-export this
-from ._safetensors_rust import safe_open, serialize, serialize_file, deserialize, SafetensorError  # noqa: F401
+from ._safetensors_rust import SafetensorError, deserialize, safe_open, serialize, serialize_file  # noqa: F401
