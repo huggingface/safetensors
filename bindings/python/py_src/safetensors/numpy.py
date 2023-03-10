6b4a870586b7dff363d0dbbdb4a5578b86577b57
Nicolas Patry <patry.nicolas@protonmail.com>
2023-03-10 22:48:38 +0100

    Forcing byte order in numpy at load time (Fixes loading a safetensor from a big endian machine).

--- bindings/python/py_src/safetensors/numpy.py
+++ bindings/python/py_src/safetensors/numpy.py
@@ -158,6 +158,7 @@ def _view2np(safeview) -> Dict[str, np.ndarray]:
     result = {}
     for k, v in safeview:
         dtype = _getdtype(v["dtype"])
+        dtype = dtype.newbyteorder("<")
         arr = np.frombuffer(v["data"], dtype=dtype).reshape(v["shape"])
         result[k] = arr
     return result
