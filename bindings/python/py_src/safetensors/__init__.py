0356bc45028b5c2d22d19dc74a311f7da15ad257
Nicolas Patry <patry.nicolas@protonmail.com>
2023-03-17 12:03:11 +0100

    Remove unused work.

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -1,36 +1,4 @@
 __version__ = "0.3.0"
-import json
-from typing import Dict
 
 # Re-export this
 from ._safetensors_rust import SafetensorError, deserialize, safe_open, serialize, serialize_file  # noqa: F401
-
-
-def metadata(data: bytes) -> Dict[str, str]:
-    """
-    Loads the metadata of a safetensors byte representation
-
-    Args:
-        data (`bytes`):
-            The content of a safetensors file
-
-    Returns:
-        `Dict[str, str]`: dictionary of the metadata in the file
-
-    Example:
-
-    ```python
-    import safetensors
-
-    file_path = "./my_folder/bert.safetensors"
-    with open(file_path, "rb") as f:
-        data = f.read()
-
-    metadata = safetensors.metadata(data)
-    ```
-    """
-    n_header = data[:8]
-    n = int.from_bytes(n_header, "little")
-    metadata_bytes = data[8 : 8 + n]
-    header = json.loads(metadata_bytes)
-    return header.get("__metadata__", {})
