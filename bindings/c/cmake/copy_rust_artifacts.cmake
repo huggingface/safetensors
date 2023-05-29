1dfa6c7c25114b0bb3c080057dbfbb43c86fbada
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-05-29 19:11:45 +0200

    Add support for copying library on linux (prefix [lib]safetensors)

--- bindings/c/cmake/copy_rust_artifacts.cmake
+++ bindings/c/cmake/copy_rust_artifacts.cmake
@@ -7,7 +7,7 @@ list(LENGTH SAFETENSORS_ARTIFACTS SAFETENSORS_ARTIFACTS_LEN)
 foreach (SAFETENSORS_ARTIFACT IN LISTS SAFETENSORS_ARTIFACTS)
     # Sometime we get .lib file on Windows prefixed with .dll (.dll.lib), let's remove the .dll
     string(REGEX REPLACE "(.*)/safetensors.dll.(.*)$" "\\1/safetensors.\\2" SAFETENSORS_ARTIFACT_CLEAN_NAME ${SAFETENSORS_ARTIFACT})
-    string(REGEX REPLACE "(.*)/(.*)/safetensors.(.*)$" "\\1/safetensors.\\3" SAFETENSORS_ARTIFACT_TRIM_DEST ${SAFETENSORS_ARTIFACT_CLEAN_NAME})
+    string(REGEX REPLACE "(.*)/(.*)/[lib]?safetensors.(.*)$" "\\1/safetensors.\\3" SAFETENSORS_ARTIFACT_TRIM_DEST ${SAFETENSORS_ARTIFACT_CLEAN_NAME})
     message(STATUS "Copying file ${SAFETENSORS_ARTIFACT} to ${SAFETENSORS_ARTIFACT_TRIM_DEST}")
 
     file(COPY_FILE ${SAFETENSORS_ARTIFACT} ${SAFETENSORS_ARTIFACT_TRIM_DEST} ONLY_IF_DIFFERENT)
