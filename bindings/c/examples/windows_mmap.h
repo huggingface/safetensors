60445ec6269a61c19126b960a68c06f75b7bb81b
Nicolas Patry <patry.nicolas@protonmail.com>
2023-06-08 23:45:17 +0200

    Omg.

--- bindings/c/examples/windows_mmap.h
+++ bindings/c/examples/windows_mmap.h
@@ -1,25 +1,34 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
-#define MAX_FILE_SIZE 0x10000000
 
 const uint8_t* map_file(const char* filename, size_t& length){
+  HANDLE file;
   HANDLE hFile;
   LPCTSTR pBuf;
-  DWORD length;
   LPVOID lpMapAddress;
 
   // Create the test file. Open it "Create Always" to overwrite any
   // existing file. The data is re-created below
   // mapping = CreateFileMappingW(handle, ptr::null_mut(), protect, 0, 0, ptr::null());
+  file = ::CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+
+  length = GetFileSize(file,  NULL);
+  if (length <= 0){
+    printf("Cannot open file : %s\n", filename);
+    printf("Try downloading a file on the hub\n");
+    printf("wget https://huggingface.co/gpt2/resolve/main/model.safetensors");
+    exit(1);
+  }
+
   hFile = CreateFileMapping(
-    INVALID_HANDLE_VALUE,
+    file,
     NULL,
     PAGE_READONLY,
+    FILE_MAP_READ,
     0,
-    MAX_FILE_SIZE,
-    filename);               // name of mapping object
-                             //
+    length,
+    NULL);
   if (hFile == NULL)
   {
     printf("Cannot open file : %s\n", filename);
@@ -28,19 +37,12 @@ const uint8_t* map_file(const char* filename, size_t& length){
     exit(1);
   }
 
-  length = GetFileSize(hFile,  NULL);
-  if (length <= 0){
-    printf("Cannot open file : %s\n", filename);
-    printf("Try downloading a file on the hub\n");
-    printf("wget https://huggingface.co/gpt2/resolve/main/model.safetensors");
-    exit(1);
-  }
 
   lpMapAddress = MapViewOfFile(hFile,
                                FILE_MAP_READ,
                                0,
                                0,
-                               MAX_FILE_SIZE);
+                               0);
                                               
   if (lpMapAddress == NULL)
   {
