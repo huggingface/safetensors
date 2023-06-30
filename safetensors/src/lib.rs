b4b0fa380a9ae0f15c4c92094e8d4f86995a14f8
Nicolas Patry <patry.nicolas@protonmail.com>
2023-06-30 09:55:11 +0200

    Update other readmes.

--- safetensors/src/lib.rs
+++ safetensors/src/lib.rs
@@ -55,7 +55,7 @@
 //! - 8 bytes: `N`, a u64 int, containing the size of the header
 //! - N bytes: a JSON utf-8 string representing the header.
 //!   - The header is a dict like `{"TENSOR_NAME": {"dtype": "F16", "shape": [1, 16, 256], "data_offsets": [BEGIN, END]}, "NEXT_TENSOR_NAME": {...}, ...}`, where offsets point to the tensor data relative to the beginning of the byte buffer, with `BEGIN` as the starting offset and `END` as the one-past offset (so total tensor byte size = `END - BEGIN`).
-//!   - A special key `__metadata__` is allowed to contain free form text-to-text map.
+//!   - A special key `__metadata__` is allowed to contain free form string-to-string map. Arbitrary JSON is not allowed, all values must be strings.
 //! - Rest of the file: byte-buffer.
 //!
 //! Notes:
