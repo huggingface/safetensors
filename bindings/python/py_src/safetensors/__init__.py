f6dd8715f2bce83a70245c23fbbc1b565a09a42d
Nicolas Patry <patry.nicolas@protonmail.com>
2023-08-07 15:46:19 +0200

    Creating 0.3.2.rc1 release (#311)

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.3.dev0"
+__version__ = "0.3.2.rc1"
 
 # Re-export this
 from ._safetensors_rust import SafetensorError, deserialize, safe_open, serialize, serialize_file  # noqa: F401
