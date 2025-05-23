7eb24e3da505f34faa03ff200bd672524224219e
Nicolas Patry <patry.nicolas@protonmail.com>
2025-05-23 16:28:04 +0200

    Adding S3.

--- bindings/python/tests/test_handle.py
+++ bindings/python/tests/test_handle.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from safetensors import safe_open_handle
-from safetensors.numpy import save_file
+from safetensors.numpy import save_file, save
 
 
 class ReadmeTestCase(unittest.TestCase):
@@ -23,6 +23,42 @@ class ReadmeTestCase(unittest.TestCase):
         # Now loading
         loaded = {}
         with open("./out_np.safetensors", "r") as f:
+            with safe_open_handle(f, framework="np", device="cpu") as g:
+                for key in g.keys():
+                    loaded[key] = g.get_tensor(key)
+        self.assertTensorEqual(tensors, loaded, np.allclose)
+
+    def test_fsspec(self):
+        import fsspec
+
+        tensors = {"a": np.zeros((2, 2)), "b": np.zeros((2, 3), dtype=np.uint8)}
+
+        fs = fsspec.filesystem("file")
+        byts = save(tensors)
+        with fs.open("fs.safetensors", "wb") as f:
+            f.write(byts)
+        # Now loading
+        loaded = {}
+        with fs.open("fs.safetensors", "rb") as f:
+            with safe_open_handle(f, framework="np", device="cpu") as g:
+                for key in g.keys():
+                    loaded[key] = g.get_tensor(key)
+        self.assertTensorEqual(tensors, loaded, np.allclose)
+
+    @unittest.skip("Will not work without s3 access")
+    def test_fsspec_s3(self):
+        import s3fs
+
+        tensors = {"a": np.zeros((2, 2)), "b": np.zeros((2, 3), dtype=np.uint8)}
+
+        s3 = s3fs.S3FileSystem(anon=True)
+        byts = save(tensors)
+        print(s3.ls("my-bucket"))
+        with s3.open("out/fs.safetensors", "wb") as f:
+            f.write(byts)
+        # Now loading
+        loaded = {}
+        with s3.open("out/fs.safetensors", "rb") as f:
             with safe_open_handle(f, framework="np", device="cpu") as g:
                 for key in g.keys():
                     loaded[key] = g.get_tensor(key)
