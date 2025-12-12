4170cad25794e46d2436460c9e2a05dd07c03255
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-12-12 15:37:54 +0600

    [feat]: link gds for cuda into build system;

---
+++ bindings/python/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Try linking necessary CUDA libraries on Linux for GDS
+    #[cfg(target_os = "linux")]
+    {
+        // Add CUDA library paths for linking
+        println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
+        println!("cargo:rustc-link-search=/usr/local/cuda-12/lib64");
+        println!("cargo:rustc-link-search=/usr/local/cuda-13.0/lib64");
+        println!("cargo:rustc-link-lib=cufile");
+    }
+}
