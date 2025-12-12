1204e9626b3afa3817cda5dad68486cdbe67be6f
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-12-12 14:44:17 +0600

    [cleanup]: remove gds code from python;  - https://github.com/huggingface/safetensors/pull/676/files#r2554199302  - https://github.com/huggingface/safetensors/pull/676/files#r2554204697

--- bindings/python/py_src/safetensors/__init__.py
+++ bindings/python/py_src/safetensors/__init__.py
@@ -8,6 +8,3 @@ from ._safetensors_rust import (  # noqa: F401
     serialize,
     serialize_file,
 )
-
-# Export GDS support status for programmatic checking
-from .torch import _is_gds_available  # noqa: F401
