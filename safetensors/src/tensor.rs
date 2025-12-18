987b7380f85db6f92214d9ed0f0d36ef0e71d5f6
Luc Georges <McPatate@users.noreply.github.com>
2025-12-18 16:44:50 +0100

    feat: add direct io write fast path on macos (#687)

--- safetensors/src/tensor.rs
+++ safetensors/src/tensor.rs
@@ -307,14 +307,32 @@ fn buffered_write_to_file<V: View>(
     n: u64,
     header_bytes: &[u8],
     tensors: &[V],
+    total_size: usize,
 ) -> Result<(), SafeTensorError> {
-    let mut f = std::io::BufWriter::new(std::fs::File::create(path)?);
+    let file = std::fs::File::create(path)?;
+
+    file.set_len(total_size as u64)?;
+
+    // Serialize tensors to a file using direct I/O (bypassing page cache) using F_NOCACHE.
+    // This yields ~30% performance improvement.
+    #[cfg(target_os = "macos")]
+    unsafe {
+        use std::os::fd::AsRawFd;
+
+        libc::fcntl(file.as_raw_fd(), libc::F_NOCACHE, 1);
+    }
+
+    let mut f = std::io::BufWriter::with_capacity(1024 * 1024, file);
+
     f.write_all(n.to_le_bytes().as_ref())?;
     f.write_all(header_bytes)?;
+
     for tensor in tensors {
         f.write_all(tensor.data().as_ref())?;
     }
+
     f.flush()?;
+
     Ok(())
 }
 
@@ -334,7 +352,10 @@ where
 {
     let (
         PreparedData {
-            n, header_bytes, ..
+            n,
+            header_bytes,
+            offset,
+            ..
         },
         tensors,
     ) = prepare(data, data_info)?;
@@ -343,7 +364,9 @@ where
         return Err(SafeTensorError::HeaderTooLarge);
     }
 
-    buffered_write_to_file(filename, n, &header_bytes, &tensors)?;
+    let total_size = N_LEN + header_bytes.len() + offset;
+
+    buffered_write_to_file(filename, n, &header_bytes, &tensors, total_size)?;
 
     Ok(())
 }
